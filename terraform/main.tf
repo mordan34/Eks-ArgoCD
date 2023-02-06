a1435c50dc440a3f05d5db9d089c8173400ed645
Mor Danino <mor.danino@tikalk.com>
2023-02-06 10:56:12 +0200

    Add variable node_type

--- terraform/main.tf
+++ terraform/main.tf
@@ -61,7 +61,7 @@ module "eks" {
     one = {
       name = "node-group-1"
 
-      instance_types = ["t2.micro"]
+      instance_types = [var.node_type]
 
       min_size     = 1
       max_size     = 3
@@ -71,7 +71,7 @@ module "eks" {
     two = {
       name = "node-group-2"
 
-      instance_types = ["t2.micro"]
+      instance_types = [var.node_type]
 
       min_size     = 1
       max_size     = 2
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,9 @@ variable "region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "node_type" {
+  description = "EC2 instance type for EKS nodes"
+  type = string
+  default = "t2.micro"
+}
