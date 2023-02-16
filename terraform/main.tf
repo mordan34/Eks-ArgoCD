14092c443ccae93b2003742868259537aa9f68d1
Mor Danino <mor.danino@tikalk.com>
2023-02-16 14:31:19 +0200

    Add dependencies

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,4 +47,8 @@ module "load_balancer_controller" {
   cluster_identity_oidc_issuer     = module.eks.cluster_oidc_issuer_url
   cluster_identity_oidc_issuer_arn = module.eks.oidc_provider_arn
   cluster_name                     = module.eks.cluster_name
+
+  depends_on = [
+        module.vpc
+  ]
 }
--- terraform/prometheus.tf
+++ terraform/prometheus.tf
@@ -1,3 +1,4 @@
+# Deploying the Prometheus Operator
 data "kubectl_file_documents" "prometheus" {
     content = file("../manifests/argocd/prometheus-operator.yaml")
 }
@@ -8,5 +9,19 @@ resource "kubectl_manifest" "prometheus" {
     ]
     count     = length(data.kubectl_file_documents.prometheus.documents)
     yaml_body = element(data.kubectl_file_documents.prometheus.documents, count.index)
+    override_namespace = "argocd"
+}
+
+# Exposing Prometheus as NodePort
+data "kubectl_file_documents" "prometheus_expose" {
+    content = file("../manifests/argocd/prometheus-expose.yaml")
+}
+
+resource "kubectl_manifest" "prometheus_expose" {
+    depends_on = [
+      kubectl_manifest.prometheus
+    ]
+    count     = length(data.kubectl_file_documents.prometheus_expose.documents)
+    yaml_body = element(data.kubectl_file_documents.prometheus_expose.documents, count.index)
     override_namespace = "argocd"
 }
