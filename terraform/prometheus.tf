16fef1c33b3a5e1d67a14faa4fdcb002a6630c69
Mor Danino <mor.danino@tikalk.com>
2023-02-19 18:12:20 +0200

    Remove expose service & ingress

--- terraform/prometheus.tf
+++ terraform/prometheus.tf
@@ -11,17 +11,3 @@ resource "kubectl_manifest" "prometheus" {
     yaml_body = element(data.kubectl_file_documents.prometheus.documents, count.index)
     override_namespace = "argocd"
 }
-
-# Exposing Prometheus as NodePort
-data "kubectl_file_documents" "prometheus_expose" {
-    content = file("../manifests/argocd/prometheus-expose.yaml")
-}
-
-resource "kubectl_manifest" "prometheus_expose" {
-    depends_on = [
-      kubectl_manifest.prometheus
-    ]
-    count     = length(data.kubectl_file_documents.prometheus_expose.documents)
-    yaml_body = element(data.kubectl_file_documents.prometheus_expose.documents, count.index)
-    override_namespace = "argocd"
-}
