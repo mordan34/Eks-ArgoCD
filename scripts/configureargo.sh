a754961fcc06133c3e603df5620a6d94fc5f3331
Mor Danino <mor.danino@tikalk.com>
2023-02-19 17:29:40 +0200

    Add nginx.yaml

--- scripts/configureargo.sh
+++ scripts/configureargo.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ARGO_PWD=`kubectl -n argocd get secret argocd-secret -o jsonpath="{.data.password}" | base64 -d`
-argocd login --insecure --server argocd.tikaldev.click --username admin --password $ARGO_PWD --grpc-web
+argocd login --insecure argocd.tikaldev.click --username admin --password $ARGO_PWD --grpc-web
 
 CONTEXT_NAME=`kubectl config view -o jsonpath='{.current-context}'`
 
---
+++ terraform/nginx.tf
@@ -0,0 +1,13 @@
+# Deploying the nginx Application
+data "kubectl_file_documents" "nginx" {
+    content = file("../manifests/argocd/nginx.yaml")
+}
+
+resource "kubectl_manifest" "nginx" {
+    depends_on = [
+      kubectl_manifest.ingress_argocd
+    ]
+    count     = length(data.kubectl_file_documents.nginx.documents)
+    yaml_body = element(data.kubectl_file_documents.nginx.documents, count.index)
+    override_namespace = "argocd"
+}
