40fa3052bb80e9d7718850aead545e1837cc5240
Mor Danino <mor.danino@tikalk.com>
2023-02-20 17:11:25 +0200

    Fix Prometheus operator

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -21,7 +21,7 @@ module "eks" {
       instance_types = [var.node_type]
 
       min_size     = 1
-      max_size     = 2
+      max_size     = 3
       desired_size = 2
     }
   }
--- terraform/prometheus.tf
+++ terraform/prometheus.tf
@@ -4,10 +4,11 @@ data "kubectl_file_documents" "prometheus" {
 }
 
 resource "kubectl_manifest" "prometheus" {
-    depends_on = [
-      kubectl_manifest.ingress_argocd
-    ]
     count     = length(data.kubectl_file_documents.prometheus.documents)
     yaml_body = element(data.kubectl_file_documents.prometheus.documents, count.index)
     override_namespace = "argocd"
+
+    depends_on = [
+      kubectl_manifest.ingress_argocd
+    ]
 }
